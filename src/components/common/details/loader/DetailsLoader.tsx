01590c0f5aaf1d8c87feec2694660f68b69613b0
Gerwin Baula <baulagerwin@gmail.com>
2023-04-06 09:13:49 +0800

    fixed: details loader animation

--- src/components/common/details/loader/DetailsLoader.tsx
+++ src/components/common/details/loader/DetailsLoader.tsx
@@ -1,9 +1,9 @@
 function DetailsLoader() {
   return (
-    <>
+    <div className="u__animation--pulse">
       <div className="details-loader--desktop"></div>
       <div className="details-loader--mobile"></div>
-    </>
+    </div>
   );
 }
 
